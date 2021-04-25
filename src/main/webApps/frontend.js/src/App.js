1aa9cc3e2f012069dd605697f78670054f0384ed
samarth <samar15>
2021-04-25 23:56:28 +0530

    frontend update

--- src/main/webApps/frontend.js/src/App.js
+++ src/main/webApps/frontend.js/src/App.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 import './App.css';
-
+import NavigationBar from './components/NavigationBar'
+import LoginPage from './pages/LoginPage'
 function App() {
+
+  const marginTop={
+marginTop:"50px"
+  };
   return (
     <div className="App">
-     <p>welcome to shopping site</p>
+      <NavigationBar/>
+      <Container>
+        <Row>
+          <Col lg={12} style={marginTop}>
+            <LoginPage/>
+         </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
