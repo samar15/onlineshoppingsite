f8b7fc1ebf9005280c7683f7b1ef7a2c0be4e2f5
samarth <samar15>
2021-05-02 00:22:33 +0530

    login page ui

--- src/main/webApps/frontend.js/src/App.js
+++ src/main/webApps/frontend.js/src/App.js
@@ -1,31 +1,10 @@
 import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
 import './App.css';
-import NavigationBar from './components/NavigationBar'
 import LoginPage from './pages/LoginPage'
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 function App() {
 
-  const marginTop={
-marginTop:"50px"
-  };
   return (
-    <Router>
-      <NavigationBar/>
-      <Container>
-        <Row>
-          <Col lg={12} style={marginTop}>
-           <Switch>
-            <Route path="/about" exact component={LoginPage}/>
-            <Route path="/products" exact component={LoginPage}/>
-            <Route path="/contact" exact component={LoginPage}/>
-            <Route path="/login" exact component={LoginPage}/>
-            <Route path="/register" exact component={LoginPage}/>
-           </Switch>
-         </Col>
-        </Row>
-      </Container>
-    </Router>
+     <LoginPage/>
   );
 }
 
